5311f6b5d55c54115711624d4c75a29aeb6973db
sertaakovaa <sertaakovaa@gmail.com>
2025-04-14 17:36:29 +0300

    Обновил содержимое User.py

--- user/user.py
+++ user/user.py
@@ -11,7 +11,6 @@ class User:
         self.dislikes = dislikes
 
     def to_dict(self):
-        """Конвертирует объект User в словарь для сериализации в JSON"""
         return {
             "id": self.id,
             "name": self.name,
@@ -21,10 +20,9 @@ class User:
 
     @staticmethod
     def read_file():
-        """Считывает пользователей из файла и возвращает список объектов User"""
         users = []
         try:
-            with open(User.FILE_PATH, "r", encoding="utf-8") as f:  # Используем динамический путь
+            with open(User.FILE_PATH, "r", encoding="utf-8") as f:
                 content = f.read()
                 temp = json.loads(content) if content else []
             for item in temp:
@@ -34,25 +32,24 @@ class User:
             return []
         return users
 
-
     @staticmethod
     def add_user(user):
-        """Добавляет нового пользователя в файл users.json"""
         users = User.read_file()
         users.append(user)
-        with open(User.FILE_PATH, "w", encoding="utf-8") as f:  # Используем динамический путь к файлу
+        with open(User.FILE_PATH, "w", encoding="utf-8") as f:
             json.dump([u.to_dict() for u in users], f, indent=4, ensure_ascii=False)
 
     @staticmethod
     def get_uniq_id():
-        """Ищет уникальный id"""
         users = User.read_file()
-        id = len(users) + 1
-        return id
+        existing_ids = {user.id for user in users}
+        new_id = 1
+        while new_id in existing_ids:
+            new_id += 1
+        return new_id
 
     @staticmethod
     def get_user_by_id(id):
-        """Возвращает пользователя по id"""
         users = User.read_file()
         for user in users:
             if user.id == id:
@@ -61,10 +58,3 @@ class User:
 
     def __str__(self):
         return f"{self.id}  {self.name} {self.likes} {self.dislikes}"
-
-# user = User(User.get_uniq_id(), 'Test test', ['sports', 'travel'], ['fast food', 'politics'])
-# User.add_user(user)
-#
-# users = User.read_file()
-# for user in users:
-#     print(user)
