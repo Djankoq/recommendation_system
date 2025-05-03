7a286a15a6d6254f1e89428c6f1cd54a5ec3b280
ragemode <brodagin11@gmail.com>
2025-05-03 20:41:16 +0500

    recommendation_system

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from items.position import Position
+# from items.position import Position
 from user.user import User
 
+# def main_logic():
+#     positions = Position.get_recommend_position(44)
+#     for position in positions:
+#         print(position)
 
 def main_logic():
-    positions = Position.get_recommend_position(44)
-    for position in positions:
-        print(position)
-
+    User.add_viewed_position(44, 11)
 
 if __name__ == "__main__":
     main_logic()
